3aa6d9ff7d4ebcb61934b1d59db19c4fa076fb3e
davies-maina <daviesmaina@outlook.com>
2021-01-29 11:38:31 +0300

    products with fetch hook

--- app/Http/Resources/ProductMoreDetailsResource.php
+++ app/Http/Resources/ProductMoreDetailsResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Resources\ProductIndexResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\ProductIndexResource;
 
 class ProductMoreDetailsResource extends ProductIndexResource
 {
