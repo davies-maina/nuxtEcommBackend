01a34e23041f9df493e16c97c057857607258457
davies-maina <daviesmaina@outlook.com>
2021-01-30 17:37:27 +0300

    CORS support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \Barryvdh\Cors\HandleCors::class,
+        \App\Http\Middleware\corsMiddleware::class
+
     ];
 
     /**
@@ -60,6 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors'=> \App\Http\Middleware\CORS::class,
+
     ];
 
     /**
